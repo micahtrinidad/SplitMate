dfd807a36e7a6b7ae91be8278c619adb54ade088
Micah Trinidad <mtrinid266@gmail.com>
2026-09-26 10:50:43 -0400

    connected react frontend to backend expense api

--- demo/src/main/java/com/example/demo/ExpenseController.java
+++ demo/src/main/java/com/example/demo/ExpenseController.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.apache.catalina.connector.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -20,6 +21,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class ExpenseController {
 
     // Repositories for constructor injection
